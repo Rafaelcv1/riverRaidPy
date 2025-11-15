6c6a52481b04dba9ff9b81fe621a736c7c08638a
rafaelcv1 <rfl.must@gmail.com>
2025-11-15 00:41:36 -0300

    increase helic size

--- classes/helic.py
+++ classes/helic.py
@@ -5,7 +5,7 @@ from classes.inimigos import Inimigos
 class Helic(Inimigos):
     def __init__ (self, pos, velY, direcao):
         sprite = "sprites/helic/helic1.png"
-        dims = (32, 32)
+        dims = (40,40) 
         velx = random.randint(3,10)
         super().__init__(sprite, dims, pos, velY, direcao, velx)
 
